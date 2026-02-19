ce0c170e00d647546aba8e59228f0dc8178b3423
rhys-3 <z731014194@gmail.com>
2026-02-20 07:43:16 +0800

    fix

--- @types/function/script.d.ts
+++
@@ -1,4 +0,0 @@
-/**
- * 获取所有处于启用状态的酒馆助手脚本按钮, 主要是方便 QR 助手等兼容脚本按钮
- */
-declare function getAllEnabledScriptButtons(): { [script_id: string]: { button_id: string; button_name: string }[] };
--- @types/function/version.d.ts
+++
@@ -1,9 +0,0 @@
-/**
- * 获取酒馆助手版本号
- */
-declare function getTavernHelperVersion(): string;
-
-/**
- * 获取酒馆版本号
- */
-declare function getTavernVersion(): string;
